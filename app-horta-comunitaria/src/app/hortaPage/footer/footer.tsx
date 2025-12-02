cce18ca9fc5610c5d34bea0139f6ba5eeb7ac348
Cypher <mr9737027@gmail.com>
2025-12-01 23:40:55 -0400

    chore:mudança no texto do footer

--- app-horta-comunitaria/src/app/hortaPage/footer/footer.tsx
+++ app-horta-comunitaria/src/app/hortaPage/footer/footer.tsx
@@ -2,8 +2,7 @@ export default function Footer(){
     return(
         <footer className="bg-green-700 text-white text-center py-6 mt-12">
         <p className="text-sm">
-          © 2025 Projeto Horta Comunitária Escolar — Feito com 💚 por alunos da
-          Escola Municipal Jardim Verde
+          © Projetos 2025 — Feito com 💚 por alunos do EMEB basiliano do carmo de jesus
         </p>
       </footer>
     )
