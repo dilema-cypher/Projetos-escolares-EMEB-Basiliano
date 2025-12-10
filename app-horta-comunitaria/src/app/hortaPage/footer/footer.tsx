a3fb4260af5eb7bfa6e9e7ad9727c4744d0fdffa
Cypher <mr9737027@gmail.com>
2025-12-09 21:55:12 -0400

    fix: teste

--- app-horta-comunitaria/src/app/hortaPage/footer/footer.tsx
+++
@@ -1,9 +0,0 @@
-export default function Footer(){
-    return(
-        <footer className="bg-green-700 text-white text-center py-6 mt-12">
-        <p className="text-sm">
-          © Projetos 2025 — Feito com 💚 por alunos do EMEB basiliano do carmo de jesus
-        </p>
-      </footer>
-    )
-}
--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export", // <-- NECESSÁRIO PARA GERAR /out
 
   images: {
-    unoptimized: true, // <-- OBRIGATÓRIO para export
+    unoptimized: true, // <-- NECESSÁRIO para export
     remotePatterns: [
       {
         protocol: "https",
