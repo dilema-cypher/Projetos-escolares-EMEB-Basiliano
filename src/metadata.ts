ab4794ed672576889a9b3d0a9df151d936569f95
Cypher <mr9737027@gmail.com>
2025-12-10 22:47:12 -0400

    chore(adicionado icone do site)

--- src/metadata.ts
+++ src/metadata.ts
@@ -1,16 +1,26 @@
-import type {Metadata}from "next";
-export const meuMetadata: Metadata={
-    title:"Horta comunitaria EMEB Basiliano do Carmo de jesus edital",
-    description:"Site voltado a promoção do projeto horta comunitaria EMEB Basiliano, Um projeto educativo em que as crianças cultivam, aprendem e compartilham.",
-    applicationName:"name:'Horta comunitaria sinop/mt'",
-    authors:[
-        {url:'https://github.com/dilema-cypher', name:'Marcos silva'},
-        {name:'Inaja Maringues da Silva Chiarelli'}
-    ],
-    generator:"nextJS, Tailwind",
-    keywords:["projeto escolar", "EMEB Basiliano","escola sinop", "Inaja Maringues da Silva Chiarelli"],
-    robots:{
-        index:true,
-        follow:true,
-    },
+import type { Metadata } from "next";
+export const meuMetadata: Metadata = {
+  title: "Horta comunitaria EMEB Basiliano do Carmo de jesus edital",
+  description:
+    "Site voltado a promoção do projeto horta comunitaria EMEB Basiliano, Um projeto educativo em que as crianças cultivam, aprendem e compartilham.",
+  applicationName: "name:'Horta comunitaria sinop/mt'",
+  authors: [
+    { url: "https://github.com/dilema-cypher", name: "Marcos silva" },
+    { name: "Inaja Maringues da Silva Chiarelli" },
+  ],
+  generator: "nextJS, Tailwind",
+  keywords: [
+    "projeto escolar",
+    "EMEB Basiliano",
+    "escola sinop",
+    "Inaja Maringues da Silva Chiarelli",
+  ],
+  robots: {
+    index: true,
+    follow: true,
+  },
+
+  icons: {
+  icon: "/icons8-school-bubbles-96.ico",
+},
 }
